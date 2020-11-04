02659e29019a9f24a3ac90cebc90d22688869821
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2020-11-04 13:33:03 +0200

    week3

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,7 +10,7 @@ router.get('/', userController.user_list_get);
 router.get('/:id', userController.user_get);
 
 router.post('/', [
-  body('name', 'vähintään 3 merkkiä').isLength({min: 3}),
+  body('name', 'vähintään 3 merkkiä').isLength({min: 3}).escape(),
   body('email', 'kunnollinen sähköposti').isEmail(),
   body('passwd',
       'minimum length 8 characters, at least one capital letter').matches(

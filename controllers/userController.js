30c224e5fbb0002b3e3b40c100e3b392cb44238a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2020-11-11 14:21:20 +0200

    bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 // Controller
 'use strict';
-const { validationResult } = require('express-validator');
 const userModel = require('../models/userModel');
 
 const users = userModel.users;
@@ -17,23 +16,7 @@ const user_get = async (req, res) => {
   res.json(user);
 };
 
-const user_create_post = async (req, res) => {
-  console.log(req.body);
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  // object destructuring
-  const {name, email, passwd} = req.body;
-  const params = [name, email, passwd];
-  const cat = await userModel.addUser(params);
-  res.json({message: 'user create ok'});
-};
-
 module.exports = {
   user_list_get,
   user_get,
-  user_create_post,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,14 +9,6 @@ router.get('/', userController.user_list_get);
 
 router.get('/:id', userController.user_get);
 
-router.post('/', [
-  body('name', 'vähintään 3 merkkiä').isLength({min: 3}).escape(),
-  body('email', 'kunnollinen sähköposti').isEmail(),
-  body('passwd',
-      'minimum length 8 characters, at least one capital letter').matches(
-      '(?=.*[A-Z]).{8,}'),
-], userController.user_create_post);
-
 router.put('/', (req, res) => {
   res.send('With this endpoint you can edit users');
 });

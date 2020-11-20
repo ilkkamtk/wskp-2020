66b947369010f7183786e7c009b80d3df886ca89
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2020-11-20 21:33:52 +0200

    env

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # wop-starters
 
 Choose a branch and download as zip.
+
+### How to run
+1. npm i
+1. create .env
+   ```dotenv
+   DB_HOST=
+   DB_USER=
+   DB_PASS=
+   DB_NAME=
+   PROXY_PASS=/app
+   ```
+1. Locally run `NODE_ENV=development nodemon app.js`
+1. On the server run 
+   * `NODE_ENV=production nodemon app.js`
+   * or ``
+
--- app.js
+++ app.js
@@ -7,17 +7,38 @@ const userRoute = require('./routes/userRoute');
 const passport = require('./utils/pass.js');
 const authRoute = require('./routes/authRoute');
 const app = express();
-const port = 3000;
+let port = 3000;
+
+app.enable('trust proxy');
 
 app.use(cors());
-app.use(express.static('uploads'));
-app.use(express.static('public'));
-app.use('/thumbnails', express.static('thumbnails'));
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+  port = 8000;
+  app.use ((req, res, next) => {
+    if (req.secure) {
+      // request was via https, so do no special handling
+      next();
+    } else {
+      // if express app run under proxy with sub path URL
+      // e.g. http://www.myserver.com/app/
+      // then, in your .env, set PROXY_PASS=/app
+      // Adapt to your proxy settings!
+      const proxypath = process.env.PROXY_PASS || ''
+      // request was via http, so redirect to https
+      console.log(`https://${req.headers.host}${proxypath}${req.url}`);
+      res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
+    }
+  });
+}
+
+app.use(express.static('uploads'));
+app.use(express.static('public'));
+app.use('/thumbnails', express.static('thumbnails'));
 
 app.use('/auth', authRoute);
 app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
